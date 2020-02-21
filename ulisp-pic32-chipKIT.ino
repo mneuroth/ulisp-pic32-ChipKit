d3a13ce2bba64826b339a31d5b2a09a8f99372a2
mneuroth <mneuroth@users.noreply.github.com>
2020-02-21 10:18:07 +0100

    Removed settings for arm boards and started to add settings for chipKIT boards

--- ulisp-pic32-chipKIT.ino
+++ ulisp-pic32-chipKIT.ino
@@ -1,5 +1,7 @@
-/* uLisp ARM 3.1 - www.ulisp.com
+/* uLisp PIC32-chipKIT 3.1 - www.ulisp.com
    David Johnson-Davies - www.technoblogy.com - 20th February 2020
+   
+   Modified for pic32-chipKIT by Michael Neuroth
 
    Licensed under the MIT license: https://opensource.org/licenses/MIT
 */
@@ -15,7 +17,7 @@ const char LispLibrary[] PROGMEM = "";
 // #define printgcs
 // #define sdcardsupport
 // #define lisplibrary
-#define assemblerlist
+//#define assemblerlist
 
 // Includes
 
@@ -129,72 +131,18 @@ typedef int PinMode;
 #define BUFFERSIZE 34  // Number of bits+2
 #define RAMFUNC __attribute__ ((section (".ramfunctions")))
 
-#if defined(ARDUINO_ITSYBITSY_M0) || defined(ARDUINO_FEATHER_M0_EXPRESS)
-  #define WORKSPACESIZE 3072-SDSIZE       /* Objects (8*bytes) */
-  #define DATAFLASHSIZE 2048000           /* 2 MBytes */
-  #define SYMBOLTABLESIZE 512             /* Bytes */
-  #define CODESIZE 128                    /* Bytes */
-  #define SDCARD_SS_PIN 4
-  #define STACKDIFF 320
+#if defined(ARDUINO_ARCH_PIC32)
 
-#elif defined(ARDUINO_GEMMA_M0)
-  #define WORKSPACESIZE 3072-SDSIZE       /* Objects (8*bytes) */
-  #define SYMBOLTABLESIZE 512             /* Bytes */
-  #define CODESIZE 128                    /* Bytes */
-  #define STACKDIFF 320
+#if defined(_BOARD_PICO_HOMECOMPUTER_)
+#define PSTR(s) s
+#define PROGMEM
+#define WORKSPACESIZE 3072-SDSIZE       /* Cells (8*bytes) */
+#define SYMBOLTABLESIZE 512             /* Bytes */
+#define SDCARD_SS_PIN 9                 /* RB7 == SELECT_SD_CARD */
+#endif
 
-#elif defined(ARDUINO_METRO_M4) || defined(ARDUINO_ITSYBITSY_M4) || defined(ARDUINO_FEATHER_M4)
-  #define WORKSPACESIZE 20480-SDSIZE      /* Objects (8*bytes) */
-  #define DATAFLASHSIZE 2048000           /* 2 MBytes */
-  #define SYMBOLTABLESIZE 1024            /* Bytes */
-  #define CODESIZE 256                    /* Bytes */
-  #define STACKDIFF 320
-
-#elif defined(ARDUINO_GRAND_CENTRAL_M4)
-  #define WORKSPACESIZE 30720-SDSIZE      /* Objects (8*bytes) */
-  #define DATAFLASHSIZE 8192000           /* 8 MBytes */
-  #define SYMBOLTABLESIZE 1024            /* Bytes */
-  #define CODESIZE 256                    /* Bytes */
-  #define STACKDIFF 320
-
-#elif defined(ARDUINO_SAMD_MKRZERO)
-  #define WORKSPACESIZE 3072-SDSIZE       /* Objects (8*bytes) */
-  #define SYMBOLTABLESIZE 512             /* Bytes */
-  #define CODESIZE 128                    /* Bytes */
-  #define STACKDIFF 840
-
-#elif defined(ARDUINO_SAMD_ZERO)          /* Put this last, otherwise overrides the Adafruit boards */
-  #define WORKSPACESIZE 3072-SDSIZE       /* Objects (8*bytes) */
-  #define SYMBOLTABLESIZE 512             /* Bytes */
-  #define CODESIZE 128                    /* Bytes */
-  #define SDCARD_SS_PIN 10
-  #define STACKDIFF 320
-
-#elif defined(_VARIANT_BBC_MICROBIT_)
-  #define WORKSPACESIZE 1280              /* Objects (8*bytes) */
-  #define SYMBOLTABLESIZE 512             /* Bytes */
-  #define CODESIZE 64                     /* Bytes */
-
-#elif defined(ARDUINO_NRF52840_ITSYBITSY)
-  #define WORKSPACESIZE 20992-SDSIZE      /* Objects (8*bytes) */
-  #define DATAFLASHSIZE 2048000           /* 2 MBytes */
-  #define SYMBOLTABLESIZE 1024            /* Bytes */
-  #define CODESIZE 256                    /* Bytes */
-  #define STACKDIFF 1200
-  
-#elif defined(ARDUINO_NRF52840_CLUE)
-  #define WORKSPACESIZE 20480-SDSIZE      /* Objects (8*bytes) */
-  #define DATAFLASHSIZE 2048000           /* 2 MBytes */
-  #define SYMBOLTABLESIZE 1024            /* Bytes */
-  #define CODESIZE 256                    /* Bytes */
-  #define STACKDIFF 2048
-
-#elif defined(MAX32620)
-  #define WORKSPACESIZE 24576-SDSIZE      /* Objects (8*bytes) */
-  #define SYMBOLTABLESIZE 1024            /* Bytes */
-  #define CODESIZE 256                    /* Bytes */
-  #define STACKDIFF 320
-
+#else
+#error This sketch is intended for the PIC32 platform !
 #endif
 
 object Workspace[WORKSPACESIZE] WORDALIGNED;
